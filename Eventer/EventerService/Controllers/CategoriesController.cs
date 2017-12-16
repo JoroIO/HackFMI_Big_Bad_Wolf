ebb3072d1f5b1551d38bc23738fddba6bc9af898
Mitko <mitkosl@abv.bg>
2017-12-16 08:05:36 +0200

    Initial Commit Empty projects and Partialy working RestApi

---
+++ Eventer/EventerService/Controllers/CategoriesController.cs
@@ -0,0 +1,60 @@
+﻿using EventerService.DataAccess;
+using EventerService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace EventerService.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        // GET: api/Categories
+        public string Get()
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+            DataSet ds = dal.ExecuteDataSet("sp_get_categories");
+
+            List<Category> categories = new List<Category>();
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    Category category = new Category();
+                    category.Guid = (Guid)row[0];
+                    category.Name = (string)row[1];
+
+                    categories.Add(category);
+                }
+            }
+
+            //var jsonResult = new JavaScriptSerializer().Serialize(categories);
+            var jsonResult = JsonConvert.SerializeObject(categories);
+            return jsonResult;
+        }
+
+        // GET: api/Categories/{id}
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //// POST: api/Categories
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Categories/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Categories/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Eventer/EventerService/DataAccess/DataAccessLayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EventerService.DataAccess
+{
+    public class DataAccessLayer
+    {
+        string connectionString = @"Data Source=MITKOPC;Initial Catalog=EventerDB;Persist Security Info=True;User ID=sa;password=123456";
+            //ConfigurationManager.ConnectionStrings["EventerDB"].ConnectionString;
+
+        public DataSet ExecuteDataSet(string procedureName)
+        {
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+                command.CommandText = procedureName;
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+
+                adapter.Fill(ds);
+
+                return ds;
+            }
+        }
+
+        public DataSet ExecuteDataSet(string procedureName, List<SqlParameter> parameters)
+        {
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+                command.CommandText = procedureName;
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddRange(parameters.ToArray());
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet ds = new DataSet();
+
+                adapter.Fill(ds);
+
+                return ds;
+            }
+        }
+
+        public int ExecuteNonQueryData(string procedureName, List<SqlParameter> parameters)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = procedureName;
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddRange(parameters.ToArray());
+
+                int changedRows = command.ExecuteNonQuery();
+
+                return changedRows;
+
+            }
+        }
+    }
+}
